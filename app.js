6dd763953ac65cb224a6af795530dca2a38e3f20
lanchanashivananda <lanchanagupta@gmail.com>
2017-03-24 18:19:40 -0400

    add modules, routers

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var methodOverride = require('method-override');
+var session = require('express-session');
+
+// It is a zero-dependency module that loads environment variables from a .env file into process.env.
+require('dotenv').config()
+mongoose.connect(process.env.MONGODB_URI);
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var sessionsRoute = require('./routes/sessions')
 
 var app = express();
 
@@ -18,12 +26,31 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
+
+app.use(session({
+    secret: "thesecretcodeistopsecret",
+    resave: false,
+    saveUninitialized: false
+}));
+
 
 app.use('/', index);
+app.use('/sessions', sessionsRoute);
 app.use('/users', users);
+
+// var db = mongoose.connection;
+
+// db.on('error', (err) => {
+//     console.log(err);
+// });
+
+// db.once('open', () => {
+//     console.log('Database has been connected!');
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

77e398038ca52501de8a9536df575e2ba57f5fd1
lanchanashivananda <lanchanagupta@gmail.com>
2017-03-24 18:12:16 -0400

    Add use session file with get, detele and post router'

---
+++ routes/sessions.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router =  express.Router();
+var User = require('../models/user.js');
+var authHelper = require('../helpers/auth.js');
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login', authHelper.loginUser, (req, res) => {
+    res.redirect('/users/' + req.session.currentUser._id);
+});
+
+router.delete('/', (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/users');
+    });
+});
+
+module.exports = router;

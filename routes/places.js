f3b8bbd75d9d2e88ec71b800ec793afec17f6fdd
lanchanashivananda <lanchanagupta@gmail.com>
2017-03-26 00:31:07 -0400

    Add photots new and post method

---
+++ routes/places.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router({mergeParams: true});
+const mongoose = require('mongoose').set('debug', true);
+const Places = require('../models/places.js');
+const User = require('../models/user.js');
+const authHelper = require('../helpers/auth.js');
+
+router.get('/new', (req, res) => {
+    console.log(req.params.userId);
+    User.findById(req.params.userId)
+    .exec((err, user) => {
+        console.log('Places' + user);
+        if(err) console.log(err);
+        res.render('places/new', {
+            user: user
+        });
+    });
+});
+
+router.post('/', (req, res) => {
+    console.log(req.body.name);
+    User.findById(req.params.userId)
+    .exec((err, user) => {
+        if(err) console.log(err);
+        var newplace = new Places ({
+            name: req.body.name,
+            longitude: req.body.longitude,
+            latitude: req.body.latitude,
+            state: req.body.state,
+            country: req.body.country,
+            description: req.body.description
+        });
+        user.places.push(newplace);
+        user.save((err) => {
+            if(err) console.log(err);
+            console.log('Place is added');
+        });
+        res.redirect('/'+req.params.userId);
+    })
+})
+
+module.exports = router;

183df5b9e5fc717b400c578d36c4e9b358257cbc
lanchanashivananda <lanchanagupta@gmail.com>
2017-03-27 18:56:28 -0400

    Add login header partial

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var methodOverride = require('method-override');
 var session = require('express-session');
+var hbs = require('hbs');
 
 // It is a zero-dependency module that loads environment variables from a .env file into process.env.
 require('dotenv').config()
@@ -24,6 +25,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));

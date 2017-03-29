8b2165607089b038e8ec92479c08ac8670914ddc
lanchanashivananda <lanchanagupta@gmail.com>
2017-03-28 21:34:03 -0400

    Add comments and indent to user routes and delete commented code from sessions

--- routes/sessions.js
+++ routes/sessions.js
@@ -3,16 +3,18 @@ var router =  express.Router();
 var User = require('../models/user.js');
 var authHelper = require('../helpers/auth.js');
 
+// This will renders the login page
 router.get('/login', (req, res) => {
-    // console.log('im here');
     res.render('users/login');
 });
 
+// the check the user cradentials in auth.js file and if it validated
+// right then its gonna get current user id and redirect to main page
 router.post('/login', authHelper.loginUser, (req, res) => {
-    console.log("Sessions" + req.session.currentUser._id)
     res.redirect('/' + req.session.currentUser._id);
 });
 
+// This will kill the user session when they logout
 router.delete('/', (req, res) => {
     req.session.destroy(() => {
         res.redirect('/');
--- routes/users.js
+++ routes/users.js
@@ -5,80 +5,60 @@ var Places = require('../models/places.js');
 var Photos = require('../models/photos.js');
 var authHelper = require('../helpers/auth.js');
 
-
+// This gets the user login page
 router.get('/', (req, res) => {
     User.find({})
     .exec((err, users) => {
         if(err) console.log(err);
+
         res.render('users/login', {
             users: users
         });
     });
 });
 
+// When you click on singnup button on login page this router will be called and redirect you to the signup page
 router.get('/signup', (req, res) => {
     res.render('users/signup');
 });
 
+// It call the helpers auth file to validate the user cradentials
 router.get('/:id', authHelper.createSecure, (req, res) => {
     User.findById(req.params.id)
     .exec((err, user) => {
+        // If you try to
         if(err) {
             res.send('Invalid username or password');
         }
-        // res.render('users/show.hbs', {
-        //     user: user
-        // });
         res.render('users/index.hbs', {
+            // If if the user cratentials validated then this will call the index file
             user: user
         });
     })
 });
 
-router.get('/:id/show', authHelper.createSecure, (req, res) => {
-    User.findById(req.params.id)
-    .exec((err, user) => {
-        if(err) {
-            res.send('Invalid username or password');
-        }
-        // res.render('users/show.hbs', {
-        //     user: user
-        // });
-        res.render('users/show.hbs', {
-            user: user
-        });
-    })
-});
-
-
-
+// This router will be called while creating new user
 router.post('/', authHelper.createSecure, (req, res) => {
-    console.log(req.body.email);
- var query = { 'email' : req.body.email };
- User.findOne(query, function(err, item) {
-            console.log(err);
-            if(!item) {
-             var user = new User({
-            email: req.body.email,
-            password: res.hashedPassword,
-            username: req.body.username
+    var query = { 'email' : req.body.email };
+
+    User.findOne(query, function(err, item) {
+        if(err) console.log(err);
+        // This checks the new user details and adds only if the email id doest exist in the Database
+        if(!item) {
+            var user = new User({
+                email: req.body.email,
+                password: res.hashedPassword,
+                username: req.body.username
             });
-
-
             user.save((err, user) => {
-            if(err) console.log(err);
-            res.render('users/login');
+                if(err) console.log(err);
+                res.render('users/login');
             })
         } else {
-            res.send('alread exists');
+            res.render('users/signup');
         }
-        });
+    });
 
 });
-
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
 
 module.exports = router;

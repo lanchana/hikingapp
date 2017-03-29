9e0705ac2e40f34a8849927a054232513c601e6a
lanchanashivananda <lanchanagupta@gmail.com>
2017-03-28 21:31:53 -0400

    Deleted express index file from user

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ require('dotenv').config()
 mongoose.connect(process.env.MONGODB_URI);
 
 // Getting the router files
-var index = require('./routes/index');
+// var index = require('./routes/index');
 var users = require('./routes/users');
 var sessionsRoute = require('./routes/sessions');
 var placesRoute = require('./routes/places');
@@ -40,9 +40,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
 app.use(session({
-  secret: "thesecretcodeistopsecret",
-  resave: false,
-  saveUninitialized: false
+    secret: "thesecretcodeistopsecret",
+    resave: false,
+    saveUninitialized: false
 }));
 
 
@@ -55,20 +55,20 @@ app.use('/:placesId/photos/', photosRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
